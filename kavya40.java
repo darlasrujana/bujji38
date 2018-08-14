3181bed2701ed32dbb619854ba18da408aabdf7a
kavyakonduri <41731461+kavyakonduri@users.noreply.github.com>
2018-08-14 11:48:09 +0530

    Create kavya40.java

---
+++ kavya40.java
@@ -0,0 +1,23 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		String s="welcome to guvi training program";
+        int count=1;
+        for(int i=o;i<s.length()=1;i++)
+        {
+          if((s.charAt(i)==" ")&& (s.charAt(i+1)!=" "));
+          { 
+             count++;
+             System.out.println(" no of words in a string"+count);
+        }
+      }
+	}
+}
